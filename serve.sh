971879fd82d728420fe764da8fb50b86e007effb
Jindrich Durcak <j.durcak@seznam.cz>
2020-07-11 14:53:19 +0200

    Updated HTTP server

--- serve.sh
+++ serve.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-~/Anaconda3/envs/infor/python -m http.server
+ip_info=$(ipconfig | grep "IPv4 Address")
+IFS=':'; ip_adresses=($ip_info); unset IFS;
+target_port=8000
+echo "AZ kviz running on local network${ip_adresses[-1]}:$target_port"
+~/Anaconda3/envs/infor/python -m http.server $target_port
